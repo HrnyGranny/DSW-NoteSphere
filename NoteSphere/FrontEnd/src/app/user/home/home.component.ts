046c036623610f487ec7f5188333054ccc6ceb08
HornyGranny <Alvaro.Ruiz12@alu.uclm.es>
2024-05-08 19:42:26 +0200

    v 1.0.1

--- NoteSphere/FrontEnd/src/app/user/home/home.component.ts
+++ NoteSphere/FrontEnd/src/app/user/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { UserService } from '../services/users.service';
 
 @Component({
   selector: 'app-admin-home',
@@ -10,7 +11,7 @@ import { AuthService } from '../../services/auth.service';
 export class HomeComponent implements OnInit {
   username: string = '';
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router, private userService: UserService) { }
 
   ngOnInit(): void {
     this.username = this.authService.getUser();
@@ -31,6 +32,17 @@ export class HomeComponent implements OnInit {
   }
 
   deleteAccount(): void {
-    // Aquí puedes implementar la lógica para eliminar la cuenta del usuario
+    if (confirm('¿Estás seguro de que quieres eliminar este usuario?')) {
+      this.userService.deleteUser(this.username).subscribe(
+        () => {
+          console.log('User deleted successfully');
+          // Aquí puedes manejar la respuesta del servidor, por ejemplo, redirigir al usuario a la página de inicio de sesión
+        },
+        error => {
+          console.error(error);
+          // Aquí puedes manejar los errores, por ejemplo, mostrar un mensaje de error al usuario
+        }
+      );
+    }
   }
 }
